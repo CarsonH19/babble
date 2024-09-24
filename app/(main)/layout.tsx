6d320138ff143164609c8854038800fe92dc3709
CarsonH19 <questline.carson@gmail.com>
2024-09-24 10:27:35 +0900

    feat: sidebar & sidebar header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "@/components/sidebar";
+import MobileHeader from "@/components/mobile-header";
 import React from "react";
 
 type Props = {
@@ -5,7 +7,15 @@ type Props = {
 };
 
 const MainLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <MobileHeader /> 
+      <Sidebar className="hidden lg:flex" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="bg-red-500 h-full">{children}</div>
+      </main>
+    </>
+  );
 };
 
 export default MainLayout;
