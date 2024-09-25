d0c2b2aa88b8be66dbbc58f5f8f5b5eb181b5569
CarsonH19 <questline.carson@gmail.com>
2024-09-25 00:02:55 -0400

    feat: user progress

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,25 @@
+import { redirect } from "next/navigation";
+
 import Sidebar from "@/components/sidebar";
 import MobileHeader from "@/components/mobile-header";
 import React from "react";
+import { getUserProgress } from "@/db/queries";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const MainLayout = ({ children }: Props) => {
+const MainLayout = async ({ children }: Props) => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <>
-      <MobileHeader /> 
+      <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="max-5-[1056px] mx-auto pt-6 h-full">{children}</div>
